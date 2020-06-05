2539ac27514ef0f79b25a6d78ab8dbced971820d
Maddy <mschwa22@uwo.ca>
2020-06-05 17:31:50 -0400

    initialized data

--- anti_corr_v2.py
+++ anti_corr_v2.py
@@ -6,5 +6,33 @@ Created on Wed Jun  3 14:07:08 2020
 """
 import pandas as pd
 
-DIB_data = pd.read_excel('DIB Measurements for APO Catalog.xlsx', index_col = 0 and 1)
+DIB_data = pd.read_excel('DIB Measurements for APO Catalog.xlsx', index_col = None)
 sight_data = pd.read_excel('Info on sight lines.xlsx', index_col = 0)
+
+# Replace unnamed column headers with the names of the next line. 
+new_header = DIB_data.iloc[0]
+DIB_data.columns = new_header
+DIB_data.drop([0], axis = 0)
+
+units = input("Would you like to keep the units? ")
+if units.lower() == "no":
+    DIB_data = DIB_data.drop([0, 1], axis = 0)
+    
+    
+# Get rid of any columns that you do not want. Will ask for an input for the following columns that you would like to delete.
+# 'idx'      - the index of the DIB
+# 'Wav'      - The wavlength of the DIB
+# 'FWHM'     - The full width - half maximum of the DIB. (Given both in units of km/s and A)
+# 'xm2'      - Given in km/s and A. (Sidenote: I am not sure what this actually is)
+# 'Mea Wav'  - The mean wavlength
+# 'Mea FWHM' - The mean full width - half maximum
+# 'UL'       - (I don't really know what this is)
+# 'Mea EW'   - The mean equivalent width
+# 'Mea Err'  - The mean error for the equivalent width
+# 'Result'   - The result of the recorded data. (whether the data is good/usable or not)
+
+unwanted_columns = input("What columns would you like to get rid of? (use ',' only to seperate columns, not spaces) ")
+unwanted_columns = unwanted_columns.split(',')
+DIB_data = DIB_data.drop(unwanted_columns, axis = 1)
+
+
